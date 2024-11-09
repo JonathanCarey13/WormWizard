95c62eb4e49a953c8340b091a06c74840ebf7b6c
Jonathan Carey <jonathan.carey1995@gmail.com>
2024-11-08 21:12:00 -0500

    added wormed pendulumn physics and started left movement inputs

--- ragdoll.gd
+++ ragdoll.gd
@@ -2,3 +2,12 @@ extends Node2D
 
 #@onready var worm_body_knot: RigidBody2D = $WormBodyKnot
 #linear_velocity = Vector2.Right + 50
+@onready var body = get_tree().get_first_node_in_group("worm_body")
+
+func move_left() -> void:
+	body.linear_velocity = -50
+		
+func  _physics_process(delta: float) -> void:
+		if Input.is_action_just_released("left"):
+			move_left()
+
